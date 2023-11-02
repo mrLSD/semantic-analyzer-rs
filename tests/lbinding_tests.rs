943a130ae0851a2ae8972fca7330bb69aeaa6398
Evgeny Ukhanov <evgeny@aurora.dev>
2023-11-02 22:52:09 +0100

    Tests: extend bingind tests

--- tests/lbinding_tests.rs
+++ tests/lbinding_tests.rs
@@ -1,9 +1,12 @@
 use crate::utils::SemanticTest;
 use semantic_analyzer::ast;
-use semantic_analyzer::ast::{CodeLocation, GetLocation, GetName, Ident};
+use semantic_analyzer::ast::{CodeLocation, GetLocation, GetName, Ident, ValueName};
 use semantic_analyzer::types::block_state::BlockState;
 use semantic_analyzer::types::error::StateErrorKind;
-use semantic_analyzer::types::Binding;
+use semantic_analyzer::types::expression::{ExpressionResult, ExpressionResultValue};
+use semantic_analyzer::types::semantic::SemanticStackContext;
+use semantic_analyzer::types::types::{PrimitiveTypes, Type};
+use semantic_analyzer::types::{Binding, InnerValueName, PrimitiveValue, Value};
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -64,7 +67,102 @@ fn binding_value_not_exist() {
 }
 
 #[test]
-fn binding_value_not_mutable() {}
+fn binding_value_not_mutable() {
+    let block_state = Rc::new(RefCell::new(BlockState::new(None)));
+    let mut t = SemanticTest::new();
+    let expr = ast::Expression {
+        expression_value: ast::ExpressionValue::PrimitiveValue(ast::PrimitiveValue::U64(30)),
+        operation: None,
+    };
+    let let_binding = ast::LetBinding {
+        name: ast::ValueName::new(Ident::new("x")),
+        mutable: false,
+        value_type: Some(ast::Type::Primitive(ast::PrimitiveTypes::U64)),
+        value: Box::new(expr.clone()),
+    };
+    t.state.let_binding(&let_binding, &block_state);
+    assert!(t.is_empty_error());
+    let inner_name: InnerValueName = "x.0".into();
+    let val = Value {
+        inner_name: inner_name.clone(),
+        inner_type: Type::Primitive(PrimitiveTypes::U64),
+        mutable: false,
+        alloca: false,
+        malloc: false,
+    };
+
+    let state = block_state.borrow().context.clone().get();
+    assert_eq!(state.len(), 1);
+    assert_eq!(
+        state[0],
+        SemanticStackContext::LetBinding {
+            let_decl: val.clone(),
+            expr_result: ExpressionResult {
+                expr_type: Type::Primitive(PrimitiveTypes::U64),
+                expr_value: ExpressionResultValue::PrimitiveValue(PrimitiveValue::U64(30))
+            },
+        }
+    );
+    assert!(block_state.borrow().inner_values_name.contains(&inner_name));
+    assert_eq!(
+        block_state.borrow().values.get(&("x".into())).unwrap(),
+        &val
+    );
+    let binding = ast::Binding {
+        name: ValueName::new(Ident::new("x")),
+        value: Box::new(expr.into()),
+    };
+    t.state.binding(&binding, &block_state);
+    assert!(t.check_errors_len(1));
+    assert!(t.check_error(StateErrorKind::ValueIsNotMutable));
+}
 
 #[test]
-fn binding_value_found() {}
+fn binding_value_found() {
+    let block_state = Rc::new(RefCell::new(BlockState::new(None)));
+    let mut t = SemanticTest::new();
+    let expr = ast::Expression {
+        expression_value: ast::ExpressionValue::PrimitiveValue(ast::PrimitiveValue::U64(30)),
+        operation: None,
+    };
+    let let_binding = ast::LetBinding {
+        name: ast::ValueName::new(Ident::new("x")),
+        mutable: true,
+        value_type: Some(ast::Type::Primitive(ast::PrimitiveTypes::U64)),
+        value: Box::new(expr.clone()),
+    };
+    t.state.let_binding(&let_binding, &block_state);
+    assert!(t.is_empty_error());
+    let inner_name: InnerValueName = "x.0".into();
+    let val = Value {
+        inner_name: inner_name.clone(),
+        inner_type: Type::Primitive(PrimitiveTypes::U64),
+        mutable: true,
+        alloca: false,
+        malloc: false,
+    };
+
+    let state = block_state.borrow().context.clone().get();
+    assert_eq!(state.len(), 1);
+    assert_eq!(
+        state[0],
+        SemanticStackContext::LetBinding {
+            let_decl: val.clone(),
+            expr_result: ExpressionResult {
+                expr_type: Type::Primitive(PrimitiveTypes::U64),
+                expr_value: ExpressionResultValue::PrimitiveValue(PrimitiveValue::U64(30))
+            },
+        }
+    );
+    assert!(block_state.borrow().inner_values_name.contains(&inner_name));
+    assert_eq!(
+        block_state.borrow().values.get(&("x".into())).unwrap(),
+        &val
+    );
+    let binding = ast::Binding {
+        name: ValueName::new(Ident::new("x")),
+        value: Box::new(expr.into()),
+    };
+    t.state.binding(&binding, &block_state);
+    assert!(t.is_empty_error());
+}
