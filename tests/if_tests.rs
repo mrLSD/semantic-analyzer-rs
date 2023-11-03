3df320041348f9a11e1963489828c18af6d60932
Evgeny Ukhanov <evgeny@aurora.dev>
2023-11-04 00:27:52 +0100

    Test: if-transoform tests

--- tests/if_tests.rs
+++ tests/if_tests.rs
@@ -1,13 +1,15 @@
 use semantic_analyzer::ast;
-use semantic_analyzer::ast::Ident;
+use semantic_analyzer::ast::{CodeLocation, GetLocation, Ident};
 use semantic_analyzer::types::condition::{
-    IfBodyStatement, IfBodyStatements, IfCondition, IfStatement,
+    Condition, ExpressionCondition, ExpressionLogicCondition, IfBodyStatement, IfBodyStatements,
+    IfCondition, IfLoopBodyStatement, IfStatement, LogicCondition,
 };
+use semantic_analyzer::types::expression::Expression;
 
 mod utils;
 
 #[test]
-fn if_transform() {
+fn if_single_transform() {
     let if_condition_expr = ast::Expression {
         expression_value: ast::ExpressionValue::PrimitiveValue(ast::PrimitiveValue::F32(1.2)),
         operation: None,
@@ -22,23 +24,254 @@ fn if_transform() {
             operation: None,
         }),
     };
-    let if_body =
-        ast::IfBodyStatements::If(vec![ast::IfBodyStatement::LetBinding(let_binding.clone())]);
-    let if_statement1 = ast::IfStatement {
+    let binding = ast::Binding {
+        name: ast::ValueName::new(Ident::new("x")),
+        value: Box::new(ast::Expression {
+            expression_value: ast::ExpressionValue::PrimitiveValue(ast::PrimitiveValue::Bool(true)),
+            operation: None,
+        }),
+    };
+    let fn_call = ast::FunctionCall {
+        name: ast::FunctionName::new(Ident::new("fn1")),
+        parameters: vec![],
+    };
+    let if_statement2 = ast::IfStatement {
         condition: if_condition.clone(),
-        body: if_body,
+        body: ast::IfBodyStatements::If(vec![]),
         else_statement: None,
         else_if_statement: None,
     };
-    let if_statement1_into: IfStatement = if_statement1.into();
+    let loop_statement = ast::LoopBodyStatement::Break;
+    let return_statement = if_condition_expr.clone();
+    let if_body = ast::IfBodyStatements::If(vec![
+        ast::IfBodyStatement::LetBinding(let_binding.clone()),
+        ast::IfBodyStatement::Binding(binding.clone()),
+        ast::IfBodyStatement::FunctionCall(fn_call.clone()),
+        ast::IfBodyStatement::If(if_statement2.clone()),
+        ast::IfBodyStatement::Loop(vec![loop_statement.clone()]),
+        ast::IfBodyStatement::Return(return_statement.clone()),
+    ]);
+    let mut if_statement1 = ast::IfStatement {
+        condition: if_condition.clone(),
+        body: if_body.clone(),
+        else_statement: None,
+        else_if_statement: None,
+    };
+    if_statement1.else_statement = Some(if_body.clone());
+    if_statement1.else_if_statement = Some(Box::new(if_statement1.clone()));
+    let if_statement1_into: IfStatement = if_statement1.clone().into();
     assert_eq!(
         if_statement1_into.condition,
-        IfCondition::Single(if_condition_expr.into())
+        IfCondition::Single(if_condition_expr.clone().into())
     );
+
+    let if_let_binding_into = IfBodyStatement::LetBinding(let_binding.into());
+    let if_binding_into = IfBodyStatement::Binding(binding.into());
+    let if_fn_call_into = IfBodyStatement::FunctionCall(fn_call.into());
+    let if_if_statement2_into = IfBodyStatement::If(if_statement2.into());
+    let loop_statement_into = IfBodyStatement::Loop(vec![loop_statement.into()]);
+    let return_statement_into = IfBodyStatement::Return(return_statement.into());
     assert_eq!(
         if_statement1_into.body,
-        IfBodyStatements::If(vec![IfBodyStatement::LetBinding(let_binding.into())])
+        IfBodyStatements::If(vec![
+            if_let_binding_into.clone(),
+            if_binding_into.clone(),
+            if_fn_call_into.clone(),
+            if_if_statement2_into.clone(),
+            loop_statement_into.clone(),
+            return_statement_into.clone()
+        ])
     );
-    assert!(if_statement1_into.else_statement.is_none());
-    assert!(if_statement1_into.else_if_statement.is_none());
+    assert_eq!(
+        if_statement1_into.clone().else_statement.unwrap(),
+        IfBodyStatements::If(vec![
+            if_let_binding_into,
+            if_binding_into,
+            if_fn_call_into,
+            if_if_statement2_into,
+            loop_statement_into,
+            return_statement_into
+        ])
+    );
+    let if_compare1 = *if_statement1_into.clone().else_if_statement.unwrap();
+    let if_compare2 = IfStatement {
+        else_if_statement: None,
+        ..if_statement1_into.clone()
+    };
+    assert_eq!(if_compare1, if_compare2);
+    assert_eq!(if_statement1.location(), CodeLocation::new(1, 0));
+    // For grcov
+    format!("{if_statement1:?}");
+}
+
+#[test]
+fn if_logic_transform() {
+    let if_condition_expr = ast::Expression {
+        expression_value: ast::ExpressionValue::PrimitiveValue(ast::PrimitiveValue::F32(1.2)),
+        operation: None,
+    };
+    let if_condition_expr_into: Expression = if_condition_expr.clone().into();
+    let expr_cond1 = ast::ExpressionCondition {
+        left: if_condition_expr.clone(),
+        condition: ast::Condition::Great,
+        right: if_condition_expr.clone(),
+    };
+    let expr_cond1_into: ExpressionCondition = expr_cond1.into();
+    assert_eq!(expr_cond1_into.left, if_condition_expr_into.clone());
+    assert_eq!(expr_cond1_into.right, if_condition_expr_into.clone());
+    assert_eq!(expr_cond1_into.condition, Condition::Great);
+
+    let expr_cond2 = ast::ExpressionCondition {
+        left: if_condition_expr.clone(),
+        condition: ast::Condition::Less,
+        right: if_condition_expr.clone(),
+    };
+    let expr_cond2_into: ExpressionCondition = expr_cond2.into();
+    assert_eq!(expr_cond2_into.left, if_condition_expr_into.clone());
+    assert_eq!(expr_cond2_into.right, if_condition_expr_into.clone());
+    assert_eq!(expr_cond2_into.condition, Condition::Less);
+
+    let expr_cond3 = ast::ExpressionCondition {
+        left: if_condition_expr.clone(),
+        condition: ast::Condition::Eq,
+        right: if_condition_expr.clone(),
+    };
+    let expr_cond3_into: ExpressionCondition = expr_cond3.into();
+    assert_eq!(expr_cond3_into.left, if_condition_expr_into.clone());
+    assert_eq!(expr_cond3_into.right, if_condition_expr_into.clone());
+    assert_eq!(expr_cond3_into.condition, Condition::Eq);
+
+    let expr_cond4 = ast::ExpressionCondition {
+        left: if_condition_expr.clone(),
+        condition: ast::Condition::GreatEq,
+        right: if_condition_expr.clone(),
+    };
+    let expr_cond4_into: ExpressionCondition = expr_cond4.into();
+    assert_eq!(expr_cond4_into.left, if_condition_expr_into.clone());
+    assert_eq!(expr_cond4_into.right, if_condition_expr_into.clone());
+    assert_eq!(expr_cond4_into.condition, Condition::GreatEq);
+
+    let expr_cond5 = ast::ExpressionCondition {
+        left: if_condition_expr.clone(),
+        condition: ast::Condition::LessEq,
+        right: if_condition_expr.clone(),
+    };
+    let expr_cond5_into: ExpressionCondition = expr_cond5.into();
+    assert_eq!(expr_cond5_into.left, if_condition_expr_into.clone());
+    assert_eq!(expr_cond5_into.right, if_condition_expr_into.clone());
+    assert_eq!(expr_cond5_into.condition, Condition::LessEq);
+
+    let expr_cond6 = ast::ExpressionCondition {
+        left: if_condition_expr.clone(),
+        condition: ast::Condition::NotEq,
+        right: if_condition_expr.clone(),
+    };
+    let expr_cond6_into: ExpressionCondition = expr_cond6.into();
+    assert_eq!(expr_cond6_into.left, if_condition_expr_into.clone());
+    assert_eq!(expr_cond6_into.right, if_condition_expr_into.clone());
+    assert_eq!(expr_cond6_into.condition, Condition::NotEq);
+
+    let logic_cond_and = ast::LogicCondition::And;
+    let logic_cond_and_into: LogicCondition = logic_cond_and.into();
+    assert_eq!(logic_cond_and_into, LogicCondition::And);
+
+    let logic_cond_and = ast::LogicCondition::Or;
+    let logic_cond_and_into: LogicCondition = logic_cond_and.into();
+    assert_eq!(logic_cond_and_into, LogicCondition::Or);
+
+    let let_binding = ast::LetBinding {
+        name: ast::ValueName::new(Ident::new("x")),
+        mutable: false,
+        value_type: None,
+        value: Box::new(ast::Expression {
+            expression_value: ast::ExpressionValue::PrimitiveValue(ast::PrimitiveValue::Bool(true)),
+            operation: None,
+        }),
+    };
+    let binding = ast::Binding {
+        name: ast::ValueName::new(Ident::new("x")),
+        value: Box::new(ast::Expression {
+            expression_value: ast::ExpressionValue::PrimitiveValue(ast::PrimitiveValue::Bool(true)),
+            operation: None,
+        }),
+    };
+    let fn_call = ast::FunctionCall {
+        name: ast::FunctionName::new(Ident::new("fn1")),
+        parameters: vec![],
+    };
+
+    let if_statement2 = ast::IfStatement {
+        condition: ast::IfCondition::Single(if_condition_expr.clone()),
+        body: ast::IfBodyStatements::If(vec![]),
+        else_statement: None,
+        else_if_statement: None,
+    };
+    let loop_statement = ast::LoopBodyStatement::Break;
+    let return_statement = if_condition_expr.clone();
+    let if_loop_body = ast::IfBodyStatements::Loop(vec![
+        ast::IfLoopBodyStatement::LetBinding(let_binding.clone()),
+        ast::IfLoopBodyStatement::Binding(binding.clone()),
+        ast::IfLoopBodyStatement::FunctionCall(fn_call.clone()),
+        ast::IfLoopBodyStatement::If(if_statement2.clone()),
+        ast::IfLoopBodyStatement::Loop(vec![loop_statement.clone()]),
+        ast::IfLoopBodyStatement::Return(return_statement.clone()),
+        ast::IfLoopBodyStatement::Break,
+        ast::IfLoopBodyStatement::Continue,
+    ]);
+
+    let expr_cond = ast::ExpressionCondition {
+        left: if_condition_expr.clone(),
+        condition: ast::Condition::Eq,
+        right: if_condition_expr.clone(),
+    };
+    let mut expr_logic_cond = ast::ExpressionLogicCondition {
+        left: expr_cond.clone(),
+        right: None,
+    };
+    expr_logic_cond.right = Some((ast::LogicCondition::And, Box::new(expr_logic_cond.clone())));
+    let if_logic_condition = ast::IfCondition::Logic(expr_logic_cond.clone());
+
+    let if_statement3 = ast::IfStatement {
+        condition: if_logic_condition.clone(),
+        body: if_loop_body,
+        else_statement: None,
+        else_if_statement: None,
+    };
+    // For grcov
+    format!("{if_statement3:?}");
+    let expr_logic_cond_into: ExpressionLogicCondition = expr_logic_cond.into();
+    assert_eq!(
+        expr_logic_cond_into.left,
+        ExpressionCondition {
+            left: if_condition_expr_into.clone(),
+            condition: Condition::Eq,
+            right: if_condition_expr_into,
+        }
+    );
+    let if_statement3_into: IfStatement = if_statement3.clone().into();
+    assert_eq!(
+        if_statement3_into.condition,
+        IfCondition::Logic(expr_logic_cond_into)
+    );
+    let if_let_binding_into = IfLoopBodyStatement::LetBinding(let_binding.into());
+    let if_binding_into = IfLoopBodyStatement::Binding(binding.into());
+    let if_fn_call_into = IfLoopBodyStatement::FunctionCall(fn_call.into());
+    let if_statement2_into = IfLoopBodyStatement::If(if_statement2.into());
+    let loop_statement_into = IfLoopBodyStatement::Loop(vec![loop_statement.into()]);
+    let return_statement_into = IfLoopBodyStatement::Return(return_statement.into());
+    assert_eq!(
+        if_statement3_into.body,
+        IfBodyStatements::Loop(vec![
+            if_let_binding_into,
+            if_binding_into,
+            if_fn_call_into,
+            if_statement2_into,
+            loop_statement_into,
+            return_statement_into,
+            IfLoopBodyStatement::Break,
+            IfLoopBodyStatement::Continue,
+        ])
+    );
+    assert!(if_statement3_into.else_if_statement.is_none());
+    assert!(if_statement3_into.else_if_statement.is_none());
 }
